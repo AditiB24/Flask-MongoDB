ce2c18a3c340cb9d1833f3c89fd294bb60ba653e
Aditi Bhilare <120243818+AditiB24@users.noreply.github.com>
2024-12-15 21:38:17 +0530

    Added mongo key & value models.py

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ import os
 from pymongo import MongoClient
 
 # Fetch MongoDB URI from environment variables or use localhost as fallback
-mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+mongo_uri = os.getenv("MONGO_URI", "mongodb+srv://bhilareaditi24:<db_password>@aditib.qs572.mongodb.net/?retryWrites=true&w=majority&appName=AditiB")
 client = MongoClient(mongo_uri)
 
 # Connect to your database
