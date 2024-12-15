d04abfce586bbdab2a684eca843ba621974c6b83
Aditi Bhilare <120243818+AditiB24@users.noreply.github.com>
2024-12-15 21:30:50 +0530

    Updated for mongodb models.py

--- app/models.py
+++ app/models.py
@@ -1,4 +1,10 @@
-# app/models.py
-from flask_pymongo import PyMongo
+import os
+from pymongo import MongoClient
 
-mongo = PyMongo()
+# Fetch MongoDB URI from environment variables or use localhost as fallback
+mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+client = MongoClient(mongo_uri)
+
+# Connect to your database
+db = client["todo_app"]
+todos_collection = db["todos"]
