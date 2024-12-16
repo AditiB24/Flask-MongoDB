845aae3501c7bcc516b4b19cae484fe073128c8a
Aditi Bhilare <120243818+AditiB24@users.noreply.github.com>
2024-12-16 12:22:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
 class Config:
-    DATABASE_URL = os.getenv('postgresql://new_project_8fzg_user:LJke8zkvYQYdLnkVuhoBEfxMDQ8dNshG@dpg-ctfrlvt2ng1s738mfcag-a.singapore-postgres.render.com/new_project_8fzg')
+    DATABASE_URL = os.getenv('DATABASE_URL')
