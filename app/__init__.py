f25e0b7f0a6d347b0845e4ee1648b3638fbaeedf
Aditi Bhilare <120243818+AditiB24@users.noreply.github.com>
2024-12-16 12:03:50 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,10 @@ def create_app():
     app = Flask(__name__)
 
     # Configure PostgreSQL URI
-    app.config['DATABASE_URL'] = os.getenv('DATABASE_URL', 'postgresql://new_project_8fzg_user:LJke8zkvYQYdLnkVuhoBEfxMDQ8dNshG@dpg-ctfrlvt2ng1s738mfcag-a.singapore-postgres.render.com/new_project_8fzg')
-
+    # app.config['DATABASE_URL'] = os.getenv('DATABASE_URL', 'postgresql://new_project_8fzg_user:LJke8zkvYQYdLnkVuhoBEfxMDQ8dNshG@dpg-ctfrlvt2ng1s738mfcag-a.singapore-postgres.render.com/new_project_8fzg')
+    app.config['DATABASE_URL'] = os.getenv('DATABASE_URL')
     # Register Blueprints (routes)
     app.register_blueprint(todo_bp)
 
     return app
+
