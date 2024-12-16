4c042d30e0dbb1f342004aa13c1c83870872284d
Aditi Bhilare <120243818+AditiB24@users.noreply.github.com>
2024-12-16 11:51:07 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,13 @@
-# app/__init__.py
 from flask import Flask
-from app.models import mongo
 from app.routes import todo_bp
+import psycopg2
+import os
 
 def create_app():
     app = Flask(__name__)
 
-    # Configure MongoDB URI (Adjust this if you have a different database setup)
-    app.config['MONGO_URI'] = 'mongodb://localhost:27017/todo_db'  # Use your own MongoDB URI
-
-    mongo.init_app(app)
+    # Configure PostgreSQL URI
+    app.config['DATABASE_URL'] = os.getenv('DATABASE_URL', 'postgresql://new_project_8fzg_user:LJke8zkvYQYdLnkVuhoBEfxMDQ8dNshG@dpg-ctfrlvt2ng1s738mfcag-a.singapore-postgres.render.com/new_project_8fzg')
 
     # Register Blueprints (routes)
     app.register_blueprint(todo_bp)
